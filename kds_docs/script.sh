82cd8109b7820f5773224a6d3e6d7947c3b1b715
Mohan Narayanaswamy <mohanmca@gmail.com>
2019-08-24 13:49:00 +0800

    Cleanup after reorg folders

--- kds_docs/script.sh
+++ kds_docs/script.sh
@@ -1,11 +1,12 @@
 git clone https://github.com/kubernetes/website.git
 mkdir k8syamls
-cd website\content\en
-gfind -type f | grep yaml | xargs -i cp {} ../../../k8syamls/
+find  website/content/en -type f | xargs -i cp {} k8syamls/
+rm -rf website
 
 ## in new session
-cd ../ckad/k8syamls
+cd k8syamls
+find . -type f ! -name "*.yaml" -exec rm -rf {} \;
 
 ## is gsort -nr doesn't work?
-grep --no-filename -o "[^:]*:" *.yaml | sed -e "s/ //g" | grep -v '\"' | grep -v "#" | gsort | uniq -c | gsort -nr | tac > ../keys.txt
-grep --no-filename -o ":[^:]*" *.yaml | sed -e "s/ //g" | grep -v '\"' | grep -v "#" | gsort | uniq -c | gsort -nr | tac > ../values.txt
+grep --no-filename -o "[^:]*:" *.yaml | sed -e "s/ //g" | sed -e "s/^-//g" | grep -v '\"' | grep -v "#" | sort | uniq -c | sort -nr | grep -v "1 " | grep -v "^1" > ../keys.txt
+grep --no-filename -o ":[^:]*" *.yaml | sed -e "s/ //g" | sed -e "s/^-//g" | grep -v '\"' | grep -v "#" | sort | uniq -c | sort -nr | grep -v "1 " | grep -v ":$" | grep -v "^1" > ../values.txt
